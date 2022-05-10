2659baf659620b404b6dfb80e870c8e0c1b02b81
chrissiku <chrissiku5@gmail.com>
2022-05-10 11:43:38 +0200

    Fix arrows on for desktop verso

--- main.js
+++ main.js
@@ -1,19 +1,21 @@
-const menubar = document.querySelector('#menu');
-const hero = document.querySelector('.hero');
-const nav = document.querySelector('nav ul');
+const menubar = document.querySelector("#menu");
+const hero = document.querySelector(".hero");
+const nav = document.querySelector("nav ul");
 
-menubar.addEventListener('click', () => {
-  if (menubar.classList.contains('fa-bars')) {
-    nav.classList.toggle('showmenu');
-    menubar.classList.replace('fa-bars', 'fa-xmark');
-    hero.style.display = 'none';
+menubar.addEventListener("click", () => {
+  if (menubar.classList.contains("fa-bars")) {
+    nav.classList.toggle("showmenu");
+    menubar.classList.replace("fa-bars", "fa-xmark");
+    hero.style.display = "none";
   } else {
-    menubar.classList.replace('fa-xmark', 'fa-bars');
-    nav.classList.toggle('showmenu');
-    hero.style.display = 'block';
+    menubar.classList.replace("fa-xmark", "fa-bars");
+    nav.classList.toggle("showmenu");
+    hero.style.display = "block";
   }
 });
 
-window.addEventListener('scroll', () => {
-  document.querySelector('nav').classList.toggle('screen-scroll', window.scrollY > 0);
+window.addEventListener("scroll", () => {
+  document
+    .querySelector("nav")
+    .classList.toggle("screen-scroll", window.scrollY > 0);
 });
