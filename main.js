752a306a5f038c2bea3e29517551db73ab0d20ee
lomantech <87063237+lomantech@users.noreply.github.com>
2022-05-10 13:03:26 +0300

    add eslint configuration

--- main.js
+++ main.js
@@ -1,32 +1,28 @@
-const menubar = document.querySelector("#menu");
-const hero = document.querySelector(".hero");
-const nav = document.querySelector("nav ul");
+const menubar = document.querySelector('#menu');
+const hero = document.querySelector('.hero');
+const nav = document.querySelector('nav ul');
 
-menubar.addEventListener("click", () => {
-  if (menubar.classList.contains("fa-bars")) {
-    nav.classList.toggle("showmenu");
-    menubar.classList.replace("fa-bars", "fa-xmark");
-    hero.style.display = "none";
+menubar.addEventListener('click', () => {
+  if (menubar.classList.contains('fa-bars')) {
+    nav.classList.toggle('showmenu');
+    menubar.classList.replace('fa-bars', 'fa-xmark');
+    hero.style.display = 'none';
   } else {
-    menubar.classList.replace("fa-xmark", "fa-bars");
-    nav.classList.toggle("showmenu");
-    hero.style.display = "block";
+    menubar.classList.replace('fa-xmark', 'fa-bars');
+    nav.classList.toggle('showmenu');
+    hero.style.display = 'block';
   }
 });
 
-function hide_menu() {
-  nav.addEventListener("click", () => {
-    nav.classList.toggle("showmenu");
-    hero.style.display = "block";
-    nav.style.display = "none";
-    menubar.classList.replace("fa-xmark", "fa-bars");
-  });
-}
+nav.addEventListener('click', () => {
+  nav.classList.toggle('showmenu');
+  nav.style.display = 'hidden';
+  menubar.classList.replace('fa-xmark', 'fa-bars');
+  hero.style.display = 'block';
+});
 
-hide_menu();
-
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   document
-    .querySelector("nav")
-    .classList.toggle("screen-scroll", window.scrollY > 0);
+    .querySelector('nav')
+    .classList.toggle('screen-scroll', window.scrollY > 0);
 });
