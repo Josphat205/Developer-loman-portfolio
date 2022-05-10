268b23e70979acbfc63e0ffbf8e31361f0a8d371
lomantech <87063237+lomantech@users.noreply.github.com>
2022-05-10 12:02:51 +0300

    add responsiveness using javascript DOM

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
-document.querySelector('#menu').addEventListener('click', () => {
-  document.querySelector('nav ul').classList.toggle('showmenu');
+const menubar = document.querySelector('#menu');
+const hero = document.querySelector('.hero');
+const nav = document.querySelector('nav ul');
+
+menubar.addEventListener('click', () => {
+  if (menubar.classList.contains('fa-bars')) {
+    nav.classList.toggle('showmenu');
+    menubar.classList.replace('fa-bars', 'fa-xmark');
+    hero.style.display = 'none';
+  } else {
+    menubar.classList.replace('fa-xmark', 'fa-bars');
+    nav.classList.toggle('showmenu');
+    hero.style.display = 'block';
+  }
 });
 
 window.addEventListener('scroll', () => {
